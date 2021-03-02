03065407695dc7dc2a622d565ea8e4762baf87dc
jujubebat <bbwwpark@naver.com>
2021-03-02 22:01:19 +0900

    feat: 실패하는 테스트 케이스 추가

--- src/test/java/domain/lotto/LottoMoneyTest.java
+++ src/test/java/domain/lotto/LottoMoneyTest.java
@@ -1,6 +1,8 @@
 package domain.lotto;
 
+import static lotto.domain.lotto.LottoMoney.LOTTO_PURCHASE_MONEY_LACK_ERROR;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import lotto.domain.lotto.LottoMoney;
 import lotto.domain.lotto.PurchaseCount;
@@ -24,10 +26,27 @@ class LottoMoneyTest {
     }
 
     @Test
+    @DisplayName("로또 금액이 음수라면 예외가 발생한다.")
+    void lottoMoneyCreateException() {
+        assertThatThrownBy(() -> new LottoMoney(-1))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(LOTTO_PURCHASE_MONEY_LACK_ERROR);
+    }
+
+    @Test
     @DisplayName("로또 구매 개수만큼 금액이 차감된다.")
     void testSpendLottoLine() {
         PurchaseCount purchaseCount = new PurchaseCount(5);
         assertThat(lottoMoney.spendLottoLine(purchaseCount).getCanBuyLottoLineCount()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("로또 구매 금액이 부족하면 금액 차감시 예외가 발생한다.")
+    void testSpendLottoLineException() {
+        PurchaseCount purchaseCount = new PurchaseCount(20);
+        assertThatThrownBy(() -> lottoMoney.spendLottoLine(purchaseCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(LOTTO_PURCHASE_MONEY_LACK_ERROR);
     }
 
 }
